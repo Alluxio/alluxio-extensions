baaaa1bf9aefc6a676ab05347d9bb32485d0fba1
Cheng Chang <myairia@gmail.com>
2018-02-05 02:02:26 +0800

    Rename package to obs. (#10)

--- underfs/obs/src/test/java/alluxio/underfs/obs/OBSOutputStreamTest.java
+++ underfs/obs/src/test/java/alluxio/underfs/obs/OBSOutputStreamTest.java
@@ -9,7 +9,7 @@
  * See the NOTICE file distributed with this work for information regarding copyright ownership.
  */
 
-package alluxio.underfs.oss;
+package alluxio.underfs.obs;
 
 import com.obs.services.ObsClient;
 import com.obs.services.exception.ObsException;
